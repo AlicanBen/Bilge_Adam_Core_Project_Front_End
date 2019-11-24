c0b6ff7136bd3b5952a4a143dc7044e1ede5d541
AlicanBen <alican.ben.97@gmail.com>
2019-11-24 23:44:12 +0300

    commit #1

--- bilge_adam_core_project_front_end/src/AppRouter/AppRouter.js
+++ bilge_adam_core_project_front_end/src/AppRouter/AppRouter.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import MainPage from "../MainPage";
 
-const AppRouter = () => <BrowserRouter></BrowserRouter>;
+const AppRouter = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={MainPage} />
+  </BrowserRouter>
+);
 export default AppRouter;
